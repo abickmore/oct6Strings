7cc973f34846c73e7671f9912b22be254eaedaf1
abickmore <bickmorebusiness@yahoo.com>
2021-10-06 13:32:12 +0200

    pushing at 1:30pm, only started, still needs me to continue working

---
+++ src/main/java/Exercises.java
@@ -0,0 +1,12 @@
+package main.java;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Exercises {
+
+    public static String exercise2 (String sentence, char oldLetter, char newLetter){
+        System.out.println(exercise2());
+
+    String newSentence = newSentence.replace(oldLetter, newLetter);
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+package main.java;
+
+public class Main {
+
+    import java.util.Scanner;
+
+    public static void main(String[] args) {
+
+        //demo - scanner stuff: use scanner to read the input for every method  from the CLI
+scanner sc = new Scanner
+
+    }
+}
